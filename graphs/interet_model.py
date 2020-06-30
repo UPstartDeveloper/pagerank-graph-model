60171902f9ba47bce202be2e82aaae243a30b4eb
= <zainr7989@gmail.com>
2020-06-30 19:24:01 -0400

    Implement constructors for Internet and Page.

--- graphs/interet_model.py
+++ graphs/interet_model.py
@@ -2,11 +2,17 @@ class Page:
     """
     Representation of a single web page and its linked pages.
     """
-    def __init(self, id):
+    def __init__(self, id):
         self.page_id = id
+        # map each linked page_id --> Page obj
+        self.neighbors = dict()
+        # each link has the same weight
+        self.link_weight = 0
 
 class Internet:
     """
     Represents a composition of all Page instances in the network.
     """
-    pass
+    def __init__(self):
+        # map each page_id --> Page obj
+        self.pages = dict()
