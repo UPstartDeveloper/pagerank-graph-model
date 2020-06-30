15c6917c31c0926c72ebe7146a12693a647fa9fd
= <zainr7989@gmail.com>
2020-06-30 19:32:41 -0400

    Implement methods to link Pages together.

--- graphs/interet_model.py
+++ graphs/interet_model.py
@@ -9,6 +9,16 @@ class Page:
         # each link has the same weight
         self.link_weight = 0
 
+    def __set_link_weight(self, num_links=len(self.neighbors)):
+        self.link_weight = 1 / num_links
+
+    def add_link(self, page):
+        # recalculate the link weight
+        num_links = 1 + len(self.neighbors)
+        self.set_link_weight(num_links)
+        # add neighbor
+        self.neighbors[page.id] = page
+
 class Internet:
     """
     Represents a composition of all Page instances in the network.
