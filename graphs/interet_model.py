071595d5444b5e65c06bcee7d8aa1bef6f43b62f
= <zainr7989@gmail.com>
2020-06-30 19:14:46 -0400

    Init Page and Internet classes, main.py script.

---
+++ graphs/interet_model.py
@@ -0,0 +1,11 @@
+class Page:
+    """
+    Representation of a single web page and its linked pages.
+    """
+    pass
+
+class Internet:
+    """
+    Represents a composition of all Page instances in the network.
+    """
+    pass
